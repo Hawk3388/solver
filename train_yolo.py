51b7a1d86c135b65ec9ea61280b83f9cfc67e1b0
Hawk3388 <arvid.bouziane@icloud.com>
2026-03-19 18:04:59 +0100

    modified:   train_yolo.py

--- train_yolo.py
+++ train_yolo.py
@@ -178,7 +178,7 @@ if __name__ == "__main__":
     MODEL_SIZE = 'l'
     
     # Training-Parameter
-    EPOCHS = 1000        # Anzahl Epochen (je mehr, desto besser - aber länger)
+    EPOCHS = 1500        # Anzahl Epochen (je mehr, desto besser - aber länger)
     IMG_SIZE = 640       # Standard: 640, für hochauflösende Bilder: 1280
     BATCH_SIZE = 16      # Anpassen je nach GPU (8, 16, 32, 64)
     
